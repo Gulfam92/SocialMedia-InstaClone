c18261e58b568dae8b3ac61d1e4c8b14dc97ea3d
Gulfam92 <gulfam92@gmail.com>
2021-01-17 10:43:03 -0500

    image storing on cloud & token in local storage

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -3,7 +3,45 @@ import React from 'react';
 
 const Home = ()=>{
     return (
-        <h1>home</h1>
+        <div className= "home">
+            <div className= "card home-card">
+                <h5>Name - User 1</h5>
+                <div className= "card-image">
+                    <img src= "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTd8fHdhbGxwYXBlcnxlbnwwfDB8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"/>
+                </div>
+                <div className= "card-content">
+                    <i className="material-icons" style={{color: "red"}}>favorite</i>
+                    <h6>Title</h6>
+                    <p>This wallpaper looks great.</p>
+                    <input type= "text" placeholder= "add a comment"/>
+                </div>
+            </div>
+            <div className= "card home-card">
+                <h5>Name - User 1</h5>
+                <div className= "card-image">
+                    <img src= "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTd8fHdhbGxwYXBlcnxlbnwwfDB8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"/>
+                </div>
+                <div className= "card-content">
+                <i className="material-icons" style={{color: "red"}}>favorite</i>
+                    <h6>Title</h6>
+                    <p>This wallpaper looks great.</p>
+                    <input type= "text" placeholder= "add a comment"/>
+                </div>
+            </div>
+            <div className= "card home-card">
+                <h5>Name - User 1</h5>
+                <div className= "card-image">
+                    <img src= "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTd8fHdhbGxwYXBlcnxlbnwwfDB8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"/>
+                </div>
+                <div className= "card-content">
+                <i className="material-icons" style={{color: "red"}}>favorite</i>
+                    <h6>Title</h6>
+                    <p>This wallpaper looks great.</p>
+                    <input type= "text" placeholder= "add a comment"/>
+                </div>
+            </div>
+
+        </div>
     )
 
 }
--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -1,8 +1,41 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, {useState} from 'react';
+import {Link, useHistory} from 'react-router-dom';
+import M from 'materialize-css'
 
 
 const SignIn = ()=>{
+        const history = useHistory()
+        const [name, setName] = useState("")
+        const [password, setPassword] = useState("")
+        const [email, setEmail] = useState("")
+        const PostData = ()=>{
+            if(!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ .test(email)){
+                M.toast({html: "Invalid email", classes: "#ff1744 red accent-3"})
+                return
+            }
+            fetch("/signup", {
+                method: "post",
+                headers:{
+                    "Content-Type" : "application/json"
+                },
+                body: JSON.stringify({
+                    name,
+                    password,
+                    email
+                })
+            }).then(res=> res.json())
+            .then(data =>{
+                if(data.error){
+                    M.toast({html: data.error, classes: "#ff1744 red accent-3"})
+                }
+                else{
+                    M.toast({html: data.message, classes: "#43a047 green darken-1"})
+                    history.push('/signin')
+                }
+        }).catch(err=>{
+            console.log(err)
+        })
+    }
     return (
         <div className = "mycard">
             <div className="card auth-card input-field" >
@@ -10,26 +43,29 @@ const SignIn = ()=>{
                 <input
                     type= "text"
                     placeholder= "name"
+                    value = {name}
+                    onChange = {(e)=>setName(e.target.value)}
                 />
                 <input
                     type= "text"
                     placeholder= "email"
+                    value = {email}
+                    onChange = {(e)=>setEmail(e.target.value)}
                 />
                 <input
                     type= "text"
                     placeholder= "password"
+                    value = {password}
+                    onChange = {(e)=>setPassword(e.target.value)}
                 />
-                <button className="btn waves-effect waves-light #64b5f6 blue lighten-2">
+                <button className="btn waves-effect waves-light #64b5f6 blue darken-1"
+                onClick= {()=> PostData()}
+                >
                     Sign Up
                 </button>
                 <h6>
                     <Link to= "/Signin">Already have an account ?</Link>
-                </h6>
-
-                
-                
-                
-                
+                </h6> 
             </div>
 
         </div>
